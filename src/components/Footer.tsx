10fa6a0a5b29e53382a1a338349ae84f0d519196
Ali Feruz <aliferuzcode@gmail.com>
2024-06-23 23:49:26 +0200

    updated links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import LinkEd from '../assets/icons/linkedin_icon.png'
-import Tableu from '../assets/icons/tableau_icon.png'
 import Github from '../assets/icons/github_icon.png'
+import { IoLogoTableau } from 'react-icons/io5'
 
 
 const Footer = () => {
@@ -14,14 +14,17 @@ const Footer = () => {
         Made with Love By <span className="text-lime-500">Ali.F</span>
      </a>
      <div className='flex space-x-2 md:space-x-5 md:px-6 md:mr-4'>
-        <a href="">
+        <a href="https://www.linkedin.com/in/gabriela-arlt-a9695782/" target='_blank'
+              rel='noreferrer'>
             <img src={LinkEd} alt="icon" width={38} height={38}/>
         </a>
-        <a href="">
+        <a href="https://github.com/gabriela-arlt" target='_blank'
+              rel='noreferrer'>
             <img src={Github} alt="icon" width={38} height={38}/>
         </a>
-        <a href="">
-            <img src={Tableu} alt="icon" width={38} height={38} className='bg-white rounded-full'/>
+        <a href="https://public.tableau.com/app/profile/gabriela.arlt/vizzes" target='_blank'
+              rel='noreferrer'>
+        <IoLogoTableau className='w-10 h-10'/>
         </a>
      </div>
      </div>
